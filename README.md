e2de69b33891bc61d2302ba787bcfbc9117677cd
nitrate92 <46730268+nitrate92@users.noreply.github.com>
2022-11-28 15:09:37 -0500

    overhaul... i guess?

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # win10-regtweaks
-This repository contains Windows 10 registry tweaks that I often make use of. If you consider using the "win10-myprefs.reg" file, please read the code comments before running it on your system. It provides a few important tweaks I make use of on almost all new Windows 10 installs.
+This repository contains Windows 10 registry tweaks that I often make use of. If you consider using the "win10-myprefs.reg" file, please read the code comments before running it on your system. It provides a few important tweaks I make use of on almost all new Windows 10 installs. To see individual tweaks, look under the "registry-extras" directory.
+# Usage
+Simply double click the .reg files and click OK to apply them. There is an optional PowerShell script for removing junk UWP apps you can run from an admin PowerShell console.
---
+++ win10-tweaks.ps1
@@ -0,0 +1,19 @@
+Write-Host "This is gonna uninstall a lot of (not much but it could be important to you) UWP crap. Run at your own risk."
+Read-Host -Prompt "Press any key to continue"
+#Write-Host "Backing up list of all installed UWP applications"
+#Get-AppxPackage –AllUsers > InstalledBefore.txt
+Write-Host "Removing applications"
+Get-AppxPackage Microsoft.WindowsCamera -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneMusic -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox.TCUI -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGameOverlay -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGamingOverlay -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxIdentityProvider -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxSpeechToTextOverlay -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsMaps -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.People -AllUsers | Remove-AppxPackage
+Get-AppxPackage Microsoft.549981C3F5F10 -AllUsers | Remove-AppxPackage      #just to clarify, this is in fact the Cortana app
+Get-AppxPackage Microsoft.YourPhone -AllUsers | Remove-AppxPackage
+[console]::beep(500,500)        #it does what it says, it beeps (just as a self reminder)
+[console]::beep(500,500)
+Read-Host -Prompt "Done, press any key to exit"
