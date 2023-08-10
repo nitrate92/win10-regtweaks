61960fd1616af41bf9ad699d94a6119a21252082
scythe <hiden64@protonmail.com>
2023-08-10 23:42:48 +0000

    check for elevation

--- winget.ps1
+++ winget.ps1
@@ -1,5 +1,12 @@
 #nitrate92's winget script. This applies to a less broad amount of people, but can be good for you if you want somewhere to start from if you are creating a small set of automation scripts like I am.
 #Note: I don't know if I have to append "--accept-package-agreements" to everything but I will because it shouldn't hurt anything.
+
+$elevated = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match 'S-1-5-32-544'
+if (-not $elevated) {
+    Write-Host "This script requires administrative privileges. Please run it again in an elevated command prompt." -ForegroundColor Red
+    exit
+}
+
 Write-Host "This installs a lot of stuff. Read the code before running, and use at your own risk."
 Read-Host -Prompt "Press any key to continue"
 winget install Microsoft.PowerToys --source winget
