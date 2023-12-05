7cf1cc487fc7da7b4ca42bb5e1ecfadfe54a9ddc
HIDEN <hiden64@protonmail.com>
2023-12-05 17:06:00 -0600

    add a script for installing winget

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ This repository contains useful Windows 10 registry tweaks and post-install scri
 * Disable components of the operating system that phone home to Microsoft
 
 To see individual tweaks, look under the "registry-extras" directory.
+There are also extra scripts that are for more broad purposes than our post-install scripts. Those can be found in the "script-extras" directory.
 # Usage
 Simply double click the .reg files and click OK to apply them. Additionally, we have additional scripts that can be run from an admin PowerShell console. It's set up to install applications, and uninstall Appx packages, although they vary because we have different preferences. **Read the contents of the files before proceeding!** You are also encouraged to modify the arrays inside the script to fit your own needs.
---
+++ script-extras/install-winget.ps1
@@ -0,0 +1,16 @@
+# winget installation script. Mainly intended for LTSC, since that doesn't come with it preinstalled OOTB.
+#
+$elevated = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match 'S-1-5-32-544'
+if (-not $elevated) {
+	Write-Host "This script requires administrative privileges. Please run it again in an elevated command prompt." -ForegroundColor Red
+	exit
+}
+
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading and installing winget and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
