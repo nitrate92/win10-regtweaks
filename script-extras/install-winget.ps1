ddc415ac8c81772a664b45439035ac6346f7b649
HIDEN64 <hiden64@protonmail.com>
2023-12-05 18:19:37 -0600

    why the fuck is this nessecary

--- script-extras/install-winget.ps1
+++ script-extras/install-winget.ps1
@@ -8,9 +8,10 @@ if (-not $elevated) {
 
 $progressPreference = 'silentlyContinue'
 Write-Information "Downloading and installing winget and its dependencies..."
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.6.2771/27abf0d1afe340e7a64fb696056b2672_License1.xml -OutFile 27abf0d1afe340e7a64fb696056b2672_License1.xml
 Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
 Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
 Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Add-AppxProvisionedPackage -Online -PackagePath Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -LicensePath .\27abf0d1afe340e7a64fb696056b2672_License1.xml -Verbose
